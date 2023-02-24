12186281bb9eabff328548b564980cc3ac2cf538
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 16:39:52 +0200

    Add project files.

---
+++ Jaden Casing Strings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Jaden_Casing_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToJadenCase("pisiun yobany"));
+        }
+        public static string ToJadenCase( string phrase)
+        {
+            int count = 0;
+            string result = "";
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+
+                switch (count)
+                {
+                    case 0:
+                        if (i != 0)
+                        {
+                            result += phrase[i];
+                        }
+                        else
+                        {
+                            result += phrase[i].ToString().ToUpper();
+                        }
+                            break;
+                        
+                    default:
+                        result += phrase[i].ToString().ToUpper();
+                        count = 0;
+                        break;
+                }            
+                if (phrase[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            return result;
+        }
+    } 
+}
